7e058ebb4747458d249d32cc364c9ad251bf38c9
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2020-06-14 05:29:42 -0500

    Univariate analysis step 1 is complete

--- homework_solution_sleep_and_BMI.R
+++ homework_solution_sleep_and_BMI.R
@@ -1,19 +1,29 @@
+############################### STEP ONE: CONDUCT UNIVARIATE ANALYSES #####################################
+
 library(summarytools)
 
 lab=read.csv("BMI_Sleep.csv",na.strings=c("NA",""))
+
+#######################################################################
+# Removing the identifier column so it doesn't enter into any analyses
+#######################################################################
+
 names(lab)
 lab=lab[,c(2,3,4,5,6)]
-names(lab)
+names(lab) 
 
 colnames(lab)=c("age","race_ethnicity", "sleep_quality", "sleep_duration", "bmi")
 names(lab)
+
+######################################################################
+# Continuous Variables   
+######################################################################
 
 numericals.summary <- summarytools::descr(lab, round.digits=4)
 
 numericals.summary
 
 #Is AGE normally distributed?
-
 age.shapiro <- shapiro.test(lab$age)
 qqnorm(lab$age);qqline(lab$age)
 age.shapiro
@@ -29,35 +39,11 @@ qqnorm(lab$bmi);qqline(lab$bmi)
 bmi.shapiro
 
 
+######################################################################
+# Categorical Variables will get counts from summary  
+######################################################################
+
 summary(lab)
 
-
-
-  
-
-by(lab, lab$SLEEP.DURATION, summary)
-
-by(lab, lab$AGE, summary)
-
-zz <- plot(lab$SLEEP.DURATION, lab$AGE, main="Sleep Duration and Age",
-     xlab="Sleep Duration ", ylab="Age")
-zz
-
-hist(lab$SLEEP.DURATION, lab$age)
-
-
-
-library(ggplot2)
-sp <- ggplot(lab, aes(x=SLEEP.DURATION  , y=AGE)) + geom_point(shape=1)
-sp
-
-sp + facet_grid(RACE ~ .)
-
-
-
-hp <- ggplot(lab, aes(x=sleep_duration)) + geom_histogram(binwidth=1,colour="white")
-hp
-
-hp + facet_grid(RACE ~ SLEEP.QUALITY.RATING, scales="free_y")
 
 
---
+++ trashandscratchpad.R
@@ -0,0 +1,31 @@
+#Trash and scratchpad stuff
+
+
+
+#   
+# 
+# by(lab, lab$SLEEP.DURATION, summary)
+# 
+# by(lab, lab$AGE, summary)
+# 
+# zz <- plot(lab$SLEEP.DURATION, lab$AGE, main="Sleep Duration and Age",
+#      xlab="Sleep Duration ", ylab="Age")
+# zz
+# 
+# hist(lab$SLEEP.DURATION, lab$age)
+# 
+# 
+# 
+# library(ggplot2)
+# sp <- ggplot(lab, aes(x=SLEEP.DURATION  , y=AGE)) + geom_point(shape=1)
+# sp
+# 
+# sp + facet_grid(RACE ~ .)
+# 
+# 
+# 
+# hp <- ggplot(lab, aes(x=sleep_duration)) + geom_histogram(binwidth=1,colour="white")
+# hp
+# 
+# hp + facet_grid(RACE ~ SLEEP.QUALITY.RATING, scales="free_y")
+
